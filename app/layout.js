bbcea0b21843bc44cfed00b95f7d8dff95ab3dfb
Basitec <abdulbaasitincrypto@gmail.com>
2025-01-29 19:30:45 +0100

    seconddddfdff

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Provider } from "react-redux";
 import store from '@/store/index'
 import Head from "next/head";
@@ -27,7 +27,9 @@ const layout = ({children}) => {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#00000095] w-[100%] bg-[url("/bgImage3.jpg")] bg-center bg-cover bg-no-repeat bg-blend-darken bg-fixed `}
       >
         <Provider store={store}>
+        <Suspense>
         {children}
+        </Suspense>
         </Provider>
       </body>
     </html>
